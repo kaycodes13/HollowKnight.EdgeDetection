a859189b4b63c89a9d73356aefab622070e6671f
kaycodes13 <egad.coding@gmail.com>
2026-04-19 16:09:15 -0400

    very minor shader optimization

--- Components/EdgeDetectionPass.cs
+++ Components/EdgeDetectionPass.cs
@@ -161,12 +161,14 @@ public class EdgeDetectionPass : MonoBehaviour {
 			width = source.width / div,
 			height = source.height / div;
 
-		RenderTexture[] temp = GetEmptyTemporaryTextures(count: 3, width, height);
+		RenderTexture[] temp;
 
 		if (TryGetExcludePass(out var otherPass)) {
+			temp = GetEmptyTemporaryTextures(count: 3, width, height);
 			otherPass.RenderSilhouette(temp[2]);
 			edgeMat.SetTexture(subtractTexID, temp[2]);
 		} else {
+			temp = GetEmptyTemporaryTextures(count: 2, width, height);
 			edgeMat.SetTexture(subtractTexID, Texture2D.blackTexture);
 		}
 		edgeMat.SetColor(lineColorID, LineColor);
